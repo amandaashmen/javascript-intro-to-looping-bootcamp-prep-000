8f74f0ee8bf6e38131a12248f1a17d72c7d9ccb4
candescent-sequence-1308 <amandaashmen@gmail.com>
2020-08-08 04:19:05 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,33 @@
+function forLoop(array) {
+  for(let i = 0; i<25; i++) {
+    if (i == 1) {
+    array.push(`I am ${i} strange loop.`)
+    }
+    else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array
+}
+
+function whileLoop(n) {
+  var countdown = n;
+  while (countdown > 0) {
+    console.log(countdown)
+    countdown -= 1
+  }
+  return 'done'
+}
+
+function doWhileLoop(num) {
+  do {
+  console.log("I run once regardless.");
+} while (incrementVariable() < num);
+}
+
+function incrementVariable() {
+  let i = 0;
+  i = i + 1;
+  return i;
+}
+
